ca60ec4c3bdc9d671f4a160c5a5797af232f02ae
Petar Petkov <Petar Petkov>
2015-11-17 01:14:51 +0400

    Admin conference controller

---
+++ ConferenceScheduler/controllers/Admin/ConferencesController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: peter
+ * Date: 11/17/2015
+ * Time: 1:14 AM
+ */
+
+namespace My\ShoppingCart\Controllers\Admin;
+
+
+class ConferencesController
+{
+
+}
