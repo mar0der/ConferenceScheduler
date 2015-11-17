d6c063ad2f717235717dcc6dfeaab39e64ea6db0
Petar Petkov <Petar Petkov>
2015-11-17 19:56:01 +0400

    Admin lectures controller

---
+++ ConferenceScheduler/controllers/Admin/LecturesController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: peter
+ * Date: 11/17/2015
+ * Time: 7:55 PM
+ */
+
+namespace My\ShoppingCart\Controllers\Admin;
+
+
+class LecturesController
+{
+
+}
